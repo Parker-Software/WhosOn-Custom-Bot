6f450bec67cf05159ee18da97b6db4c8ad7c5265
Daniel Tallentire <daniel@parkersoftware.com>
2020-09-24 00:17:46 +0100

    Custom Bot version 1
    - created demo custom bot that goes with documentation
    - basic bot just reverses text string

---
+++ WhosOnBot/CustomBot.cs
@@ -0,0 +1,35 @@
+﻿using Parkersoft.WhosOn.BotConnector;
+using Parkersoft.WhosOn.BotConnector.Message;
+using System.Collections.Generic;
+
+namespace WhosOnBot
+{
+    public class CustomBot : IBot
+    {
+        public string UniqueName { get; set; }
+        public Delegates.BotMessage OnBotMessage { get; set; }
+
+        public void AddProperties(string conversationId, IDictionary<string, string> properties)
+        {
+            return;
+        }
+
+        public bool CreateConversation(string conversationId, IDictionary<string, string> properties)
+        {
+            return true;
+        }
+
+        public bool EndConversation(string conversationId)
+        {
+            return true;
+        }
+
+        public bool SendMessage(string conversationId, string message)
+        {
+            var charArr = message.ToCharArray();
+            System.Array.Reverse(charArr);
+            this.SendBotMessageAsync(conversationId, new BotTextMessage() { Text = new string(charArr) });
+            return true;
+        }
+    }
+}
---
+++ WhosOnBot/CustomBotCreator.cs
@@ -0,0 +1,20 @@
+﻿using Parkersoft.WhosOn.BotConnector;
+
+namespace WhosOnBot
+{
+    public class CustomBotCreator : IBotCreator
+    {
+        public Delegates.CreateBot DoCreateBot
+        {
+            get
+            {
+                return ((x) =>
+                {
+                    return new CustomBot();
+                });
+            }
+        }
+
+        public string BotType => "CustomBot";
+    }
+}
